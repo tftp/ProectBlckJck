8595c402137858e68615cc57bd75799c7a18a820
Oleg Goloborodko <goloborodko@inbox.ru>
2019-10-16 17:53:52 +0300

    correction mistake for diller.rb

--- diller.rb
+++ diller.rb
@@ -9,7 +9,8 @@ class Diller
 # диллер может: пропустить ход если сумма очков 17 и более
 # диллер может: добавить карту если сумма очков меньше 17
 # может быть добавлена только одна карта
-  attr_accessor :bank, :cards
+  attr_accessor :bank, :cards, :points
+  attr_reader :name
 
   def initialize
     @name = 'Computer'
