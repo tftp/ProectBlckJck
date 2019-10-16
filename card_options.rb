dabed70371d1ed545a890b50ca8a584b1de34a9c
OS <goloborodko@inbox.ru>
2019-10-16 10:42:42 +0300

    create module card_options

---
+++ card_options.rb
@@ -0,0 +1,22 @@
+module CardOption
+
+  def create_deck_of_cards
+    suit = [9824, 9827, 9829, 9830]
+    4.times do |suit_number|
+      suit_str = "" << suit[suit_number]
+      2.upto(10) do |number|
+        name = number.to_s + suit_str
+        @cards << Card.new(name, number)
+      end
+      name = 'J' + suit_str
+      @cards << Card.new(name, 10)
+      name = 'Q' + suit_str
+      @cards << Card.new(name, 10)
+      name = 'K' + suit_str
+      @cards << Card.new(name, 10)
+      name = 'T' + suit_str
+      @cards << Card.new(name, 11)
+    end
+  end
+
+end
