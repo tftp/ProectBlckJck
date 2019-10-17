acfa83d67d28fb1c94805a4c8aa7ba362ce69f94
Oleg Goloborodko <goloborodko@inbox.ru>
2019-10-17 21:57:40 +0300

    remove from module

--- card_options.rb
+++ card_options.rb
@@ -1,30 +1,28 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
-module CardOption
-  def create_deck_of_cards
-    suit = [9824, 9827, 9829, 9830]
-    4.times do |suit_number|
-      suit_str = '' << suit[suit_number]
-      2.upto(10) do |number|
-        name = number.to_s + suit_str
-        @cards << Card.new(name, number)
-      end
-      name = 'J' + suit_str
-      @cards << Card.new(name, 10)
-      name = 'Q' + suit_str
-      @cards << Card.new(name, 10)
-      name = 'K' + suit_str
-      @cards << Card.new(name, 10)
-      name = 'T' + suit_str
-      @cards << Card.new(name, 11)
+def create_deck_of_cards
+  suit = [9824, 9827, 9829, 9830]
+  4.times do |suit_number|
+    suit_str = '' << suit[suit_number]
+    2.upto(10) do |number|
+      name = number.to_s + suit_str
+      @cards << Card.new(name, number)
     end
+    name = 'J' + suit_str
+    @cards << Card.new(name, 10)
+    name = 'Q' + suit_str
+    @cards << Card.new(name, 10)
+    name = 'K' + suit_str
+    @cards << Card.new(name, 10)
+    name = 'T' + suit_str
+    @cards << Card.new(name, 11)
   end
+end
 
-  def select_card
-    @cards[rand(0..51)]
-  end
+def select_card
+  @cards[rand(0..51)]
+end
 
-  def mixed_cards
-    @cards.each { |card| card.busy = false }
-  end
+def mixed_cards
+  @cards.each { |card| card.busy = false }
 end
--- main.rb
+++ main.rb
@@ -16,35 +16,23 @@ HIDE = false
 
 print 'Желаешь сыграть, странник? Введи своё имя: '
 name = gets.chomp
-# initialization of the player
 @player = Player.new(name)
-# initialization of the dealer
 @dealer = Diller.new
-# creating a card deck
 create_deck_of_cards
-# game start
 loop do
-  # check players for
   @player.check
   @dealer.check
-  #  the Bank of the game is filled
   @bank -= 2 * RATE
-  # the player's Bank and the dealer's Bank decrease
   @player.change_bank(RATE)
   @dealer.change_bank(RATE)
-
-  # stirring the cards
   mixed_cards
   processing("\nКарты размешиваются")
-  # the player gets 2 cards, the dealer gets 2 cards
   @player.add_card select_card
   @player.add_card select_card
   @dealer.add_card select_card
   @dealer.add_card select_card
-  # player sees his cards
   @player.points_of_cards SHOW
   @dealer.points_of_cards HIDE
-  # the selection menu of the player:
   loop do
     menu_for_player
     case gets.chomp
@@ -60,11 +48,9 @@ loop do
     end
   end
 
-  # counting results, announcement of the winner
   processing("\nПосчет результатов")
   puts "\nВнимание! Результаты!"
   open_cards
-  # offer to play again, exit
   break if check_end_game?
 end
 puts 'Спасибо за игру!'
