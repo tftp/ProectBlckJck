ec9be3ceae0d249be1bf62d1118486070b6d102f
Oleg Goloborodko <goloborodko@inbox.ru>
2019-10-17 21:10:28 +0300

    cleaning card_options

--- card_options.rb
+++ card_options.rb
@@ -1,9 +1,10 @@
-module CardOption
+# frozen_string_literal: true
 
+module CardOption
   def create_deck_of_cards
     suit = [9824, 9827, 9829, 9830]
     4.times do |suit_number|
-      suit_str = "" << suit[suit_number]
+      suit_str = '' << suit[suit_number]
       2.upto(10) do |number|
         name = number.to_s + suit_str
         @cards << Card.new(name, number)
@@ -24,7 +25,6 @@ module CardOption
   end
 
   def mixed_cards
-    @cards.each {|card| card.busy = false}
+    @cards.each { |card| card.busy = false }
   end
-
 end
