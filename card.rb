4e97ab0256aecb8e5e3491085d40ebf969adc2b5
Oleg Goloborodko <goloborodko@inbox.ru>
2019-10-17 20:38:24 +0300

    cleaning card.rb

--- card.rb
+++ card.rb
@@ -1,12 +1,12 @@
-class Card
+# frozen_string_literal: true
 
+class Card
   attr_reader :name, :value
   attr_accessor :busy
 
-  def initialize (name, value)
+  def initialize(name, value)
     @name = name
     @value = value
     @busy = false
   end
-
 end
